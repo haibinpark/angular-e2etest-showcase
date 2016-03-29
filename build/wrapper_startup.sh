17c996f8f6fa8c702954404c4f131544bf49d797
zhaohaibin <zhaohaibin@6starhome.com>
2016-03-29 14:23:12 +0800

    更新启动脚本

--- build/wrapper_startup.sh
+++ build/wrapper_startup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-/usr/bin/xvfb-daemon-run
+# /usr/bin/xvfb-daemon-run
 npm start
 sleep 1
+ps aux | grep "xvfb"
 echo "run test single run"
 npm run test-single-run
 echo "run protractor"
